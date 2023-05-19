9e2bd21d7ada2e9a550ff99c182ed571b9176bb4
Ernesto Tur Laurencio <sasousuke@gmail.com>
2023-05-19 09:03:19 -0400

    Update segavupdate.sh

--- segurmatica/segavupdate.sh
+++ segurmatica/segavupdate.sh
@@ -1,32 +1,19 @@
 #!/bin/bash
 
-# Incluir sí o sí para que el script se ejecute
 export LC_ALL=C
 
-### Usando la herramienta replicator de Segurmatica para descargar las actualizaciones
-# Herramienta para descargar hacia una carpeta local una actualizacion
+SAVDIRUPDATEBASE=/opt/webftp/Segurmatica/Update
 
-# replicator -i DBid -d <directorio> [-s <servidor>][-f <proxy> [-p <puerto>]][-u <nombre usuario> -c <contrasenna>]
+replicator -i segav -d ${SAVDIRUPDATEBASE} -f 192.168.10.1 -p 3128
 
-# -i o --dbid        Identificador de la BD a copiar (Debe coincidir con el de la BD)
-# Identificadores: Producto
-# segav: Segurmatica Antivitus 1.4
-# segavEK: Segurmatica Antivitus Edicion Kaspersky 1.4
-# savUnix: SavUnix
-# -d o --directorio  Directorio donde almacenar la actualizacion (Debe existir)
-# -s o --servidor  Servidor de actualizaciones, por defecto: http://www.segurmatica.cu/descargas/actualizaciones/segav/
-# -f o --proxy             Nombre del Proxy or IP
-# -p o --puerto    Puerto del Proxy, por defecto: 3128
-# -u o --usuario   Usuario Proxy
-# -c o --contrasenna Contrasenna del Usuario Proxy
+# Posicionandonos en la ruta donde esta la actualizacion
+cd ${SAVDIRUPDATEBASE}
 
-# Ejemplos:
-# replicator -i segav -d c:\prueba\baseAv\
-# replicator --dbid segav --directorio c:\prueba\ --servidor http://www.segurmatica.cu/descargas/actualizaciones/segav/
-# replicator --dbid segav --directorio c:\prueba\ --servidor http://www.segurmatica.cu/descargas/actualizaciones/segav/ --proxy proxy.cu --usuario nombre_usuario --contrasenna contrasenna_usuario
-# replicator -i segav -d c:\prueba\ -s http://www.segurmatica.cu/descargas/actualizaciones/segav/ -f proxy.cu -p 3128 -u nombre_usuario -c contrasenna_usuario
+# Eliminando el zip anterior
+rm updateSAV.zip
 
-replicator -i segav -d /opt/webftp/Segurmatica/Update/ -f 192.168.10.1 -p 3128
+# Creando el zip
+zip -r updateSAV.zip *
 
 # Regla de oro. Si un script finaliza de forma satisfactoria emite un mensaje de OK == 0
 exit 0
