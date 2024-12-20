217baa4050df23c2352d284020b35544f55102ad
Ernesto Tur Laurencio <sasousuke@gmail.com>
2024-12-20 13:13:00 -0500

    Update segavupdate.sh
    Modificando el script para los continuo fallos de la herramienta replicator.

--- segurmatica/segavupdate.sh
+++ segurmatica/segavupdate.sh
@@ -1,30 +1,61 @@
 #!/bin/bash
-
 export LC_ALL=C
 
-# Directorio de descarga de la actualización
-SAVDIRUPDATEBASE=/opt/webftp/Segurmatica/Update
+# Directorio de descarga de la actualizacion
+SAVDIRUPDATEBASE=/srv/ftp/antivirus/sav/actualizaciones
 
-# Borrar la actualización anterior
-rm -rf ${SAVDIRUPDATEBASE}
+# Directorio de la actualizacion manual
+SAVDIRUPDATEMANUAL=/srv/ftp/antivirus/sav/actualizacion_manual
 
-# Comprobar que existe el directorio. Si no existe, crearlo.
-if [ ! -d ${SAVDIRUPDATE} ]; then
-	mkdir -p ${SAVDIRUPDATE}
-fi
+# Archivo con la actualizacion manual
+ARCHIVOZIP=${SAVDIRUPDATEMANUAL}/actualizacion-$(date +%Y%m%d).zip
 
-#Ejecutamos la aplicación Replicator con los parámetros de nuestra red
-#Poner la ruta completa para evitar errores de no encontrado la ruta del ejecutable
-/usr/sbin/replicator -i segav -d ${SAVDIRUPDATEBASE} -f 192.168.10.1 -p 3128
+SIZEFILEMIN=1024 # 1 MB en bytes
 
-#Estableciendo permisos para que cualquier usuario del sistema pueda leer el contenido de la carpeta
-chmod -R 755 ${SAVDIRUPDATEBASE}
+# Borrar la actualizacion anterior
+rm -rf ${SAVDIRUPDATEBASE}/*
 
-# Posicionandonos en la ruta donde esta la actualizacion
-cd ${SAVDIRUPDATEBASE}
+# Borrar la actualizacion anterior
+rm -rf ${SAVDIRUPDATEMANUAL}/*
 
-# Creando el zip
-zip -r updateSAV.zip *
+actions() {
+
+    # Ejecutamos la aplicacion Replicator con los parametros de nuestra red
+    # Poner la ruta completa para evitar errores de no encontrado la ruta del ejecutable
+    /usr/local/sbin/replicator -i segav -d ${SAVDIRUPDATEBASE} -f proxy.sld.cu -p 3128
+
+    #Estableciendo permisos para que cualquier usuario del sistema pueda leer el contenido de la carpeta
+    chmod -R 755 ${SAVDIRUPDATEBASE}
+
+    # Posicionandonos en la ruta donde esta la actualizacion
+    cd ${SAVDIRUPDATEBASE}
+
+    # Creando el zip
+    zip -r updateSAV.zip *
+
+    # Mover el update hacia la carpeta destino
+    mv updateSAV.zip ${ARCHIVOZIP}
+
+    #Estableciendo permisos para que cualquier usuario del sistema pueda leer el contenido de la carpeta
+    chmod -R 755 ${SAVDIRUPDATEMANUAL}
+}
+
+while true; do
+    if [ -f "$ARCHIVOZIP" ]; then
+        SIZEFILE=$(stat -c%s "$ARCHIVOZIP")
+        if [ "$SIZEFILE" -ge "$SIZEFILEMIN" ]; then
+           echo "El archivo ahora pesa 1 MB o más. Saliendo del ciclo."
+           break
+        else
+           echo "El archivo es menor de 1 MB. Ejecutando tarea..."
+           actions 
+        fi
+    else
+        echo "El archivo no existe."
+        actions 
+    fi
+    sleep 10  # Espera 10 segundos antes de verificar nuevamente
+done
 
 # Regla de oro. Si un script finaliza de forma satisfactoria emite un mensaje de OK == 0
 exit 0
