0c23de9877103213e097e7053fa9219b2432824c
Ernesto Tur Laurencio <sasousuke@gmail.com>
2025-10-04 10:21:11 -0400

    Delete Windows/CMD directory

--- Windows/CMD/activarlan.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo "Mostrando las interfaces de red"
-netsh interface show interface
-
-echo "Activar interface de red LAN"
-netsh interface set interface LAN admin=enabled
-
-pause
--- Windows/CMD/desactivarlan.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo "Mostrando las interfaces de red"
-netsh interface show interface
-
-echo "Desactivar interface de red LAN"
-netsh interface set interface LAN admin=disabled
-
-pause
